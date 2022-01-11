574c81287ef6a565fa4ad64f11e8b58d9c11e6bb
PhiAgent <princegyekyeaddai@gmail.com>
2022-01-10 19:41:54 -0600

    tip taken as percent

--- src/store/items/selector.js
+++ src/store/items/selector.js
@@ -1,10 +1,10 @@
-import {createSelector} from 'reselect';
+import { createSelector } from 'reselect';
 
 export const selectItems = state => state.items;
-export const selectTipPercentage = state => state.selectTipPercentage;
+export const selectTipPercentage = state => state.tipPercentage;
 
 export const selectSubtotal = createSelector([selectItems], items => items.reduce((sum, item) => sum + (item.price * item.quantity), 0));
 
-export const selectTipAmount = createSelector([selectSubtotal], (subtotal, tipPercentage) => subtotal * tipPercentage);
+export const selectTipAmount = createSelector([selectSubtotal, selectTipPercentage], (subtotal, tipPercentage) => subtotal * (tipPercentage/100));
 
 export const selectTotal = createSelector([selectSubtotal, selectTipAmount], (subtotal, tipAmount) => subtotal + tipAmount);
